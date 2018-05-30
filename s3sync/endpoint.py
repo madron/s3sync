d56176fdaa7828bbf6b772d98bcd85edb409da4c
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2018-05-30 10:38:45 +0200

    fixed BaseEndpoint

--- s3sync/endpoint.py
+++ s3sync/endpoint.py
@@ -1,6 +1,8 @@
 class BaseEndpoint(object):
-    def __init__(self, log_prefix=''):
+    def __init__(self, log_prefix='', verbosity=0):
         self.log_prefix = log_prefix
+        self.verbosity = verbosity
 
     def log_info(self, message):
-        print('<{}> {}'.format(self.log_prefix, message))
+        if self.verbosity > 0:
+            print('<{}> {}'.format(self.log_prefix, message))
