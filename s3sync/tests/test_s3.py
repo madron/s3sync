f0950a6e65b5547362ab4d7d486cc33a4eeb1513
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2018-06-05 19:55:14 +0200

    added test for S3Endpoint get_path and upload

--- s3sync/tests/test_s3.py
+++ s3sync/tests/test_s3.py
@@ -1,5 +1,8 @@
+import io
 import os
 import boto3
+from contextlib import redirect_stdout
+from tempfile import TemporaryDirectory
 from test.support import EnvironmentVarGuard
 from unittest import TestCase
 from moto import mock_s3
@@ -63,17 +66,62 @@ class S3EndpointGetBucketTest(TestCase):
         self.assertEqual(bucket.name, 'bucket')
 
 
-@mock_s3
 class S3EndpointUpdateKeyDataTest(TestCase):
-    def setUp(self):
-        s3 = boto3.resource('s3')
-        self.bucket = s3.create_bucket(Bucket='bucket')
-
-    def test_update_key_data(self):
-        self.bucket.put_object(Key='path/f1', Body='content')
+    @mock_s3
+    def test_ok(self):
+        bucket = boto3.resource('s3').create_bucket(Bucket='bucket')
+        bucket.put_object(Key='path/f1', Body='content')
         endpoint = S3Endpoint(base_url='default:bucket/path', includes=[''])
         endpoint.update_key_data()
         self.assertEqual(
             endpoint.key_data,
             dict(f1=dict(size=7, etag='9a0364b9e99bb480dd25e1f0284c8555')),
         )
+
+
+class S3EndpointGetPathTest(TestCase):
+    def test_1(self):
+        endpoint = S3Endpoint(base_url='default:bucket')
+        path = endpoint.get_path('f1')
+        self.assertEqual(path, 'f1')
+
+    def test_2(self):
+        endpoint = S3Endpoint(base_url='default:bucket/path')
+        path = endpoint.get_path('f1')
+        self.assertEqual(path, 'path/f1')
+
+    def test_3(self):
+        endpoint = S3Endpoint(base_url='default:bucket/path')
+        path = endpoint.get_path('d1/f1')
+        self.assertEqual(path, 'path/d1/f1')
+
+
+class S3EndpointUploadTest(TestCase):
+    @mock_s3
+    def test_ok(self):
+        bucket = boto3.resource('s3').create_bucket(Bucket='bucket')
+        endpoint = S3Endpoint(base_url='default:bucket/path', includes=[''])
+        with TemporaryDirectory() as source_dir:
+            path = os.path.join(source_dir, 'f1')
+            with open(path, 'w') as f:
+                f.write('content')
+            self.assertTrue(os.path.isfile(path))
+            endpoint.upload('f1', path)
+        obj = bucket.Object('path/f1').get()
+        self.assertEqual(obj['ETag'], '"9a0364b9e99bb480dd25e1f0284c8555"')
+        self.assertEqual(obj['ContentLength'], 7)
+        self.assertEqual(obj['Body'].read(), b'content')
+
+    @mock_s3
+    def test_no_bucket(self):
+        endpoint = S3Endpoint(base_url='default:bucket/path', includes=[''])
+        with TemporaryDirectory() as source_dir:
+            path = os.path.join(source_dir, 'f1')
+            with open(path, 'w') as f:
+                f.write('content')
+            self.assertTrue(os.path.isfile(path))
+            stdout = io.StringIO()
+            with redirect_stdout(stdout):
+                endpoint.upload('f1', path)
+        self.assertIn('ERROR <transfer> "f1" Failed to upload', stdout.getvalue())
+        self.assertIn('An error occurred (NoSuchBucket) when calling the PutObject operation', stdout.getvalue())
