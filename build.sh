8059b93aef6052de6ebacea450983b8922df4256
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2018-06-19 19:11:32 +0200

    updated readme

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # pip3 install -r requirements-build.txt
 
 rm -rf dist
-pyinstaller --hidden-import configparser --onefile -n s3sync_x86_64 main.py
+pyinstaller --hidden-import configparser --onefile -n s3sync main.py
 rm -rf build s3sync.spec
