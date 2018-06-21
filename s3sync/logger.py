3828f734946d3b4180f2daa65e2a8f6c43977b67
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2018-06-21 11:41:51 +0200

    improved logger encoding

--- s3sync/logger.py
+++ s3sync/logger.py
@@ -10,21 +10,21 @@ class Logger(object):
     def log_info(self, message, log_prefix=None):
         if self.verbosity > 0:
             log_prefix = log_prefix or self.log_prefix
-            msg = message.encode('utf-8', 'replace').decode()
+            msg = message.encode('ascii', 'replace').decode()
             print('INFO <{}> {}'.format(log_prefix, msg))
             sys.stdout.flush()
 
     def log_debug(self, message, log_prefix=None):
         if self.verbosity > 1:
             log_prefix = log_prefix or self.log_prefix
-            msg = message.encode('utf-8', 'replace').decode()
+            msg = message.encode('ascii', 'replace').decode()
             print('DEBUG <{}> {}'.format(log_prefix, msg))
             sys.stdout.flush()
 
     def log_error(self, message, error=None, log_prefix=None):
         metrics.errors.inc(1)
         log_prefix = log_prefix or self.log_prefix
-        msg = message.encode('utf-8', 'replace').decode()
+        msg = message.encode('ascii', 'replace').decode()
         print('ERROR <{}> {}'.format(log_prefix, msg))
         if error:
             print(traceback.format_exc())
